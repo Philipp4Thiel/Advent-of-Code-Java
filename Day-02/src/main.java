d01baf583ca82164b5065eb7ff1c9f54f2d98ca8
Philipp <philipp.thiel@hispeed.ch>
2021-12-02 00:39:39 +0100

    gitignore

---
+++ Day-02/src/main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws FileNotFoundException {
+        //Scanner sc = new Scanner(new File("main.in"));
+        Scanner sc = new Scanner(new File("test.in"));
+
+
+    }
+}
