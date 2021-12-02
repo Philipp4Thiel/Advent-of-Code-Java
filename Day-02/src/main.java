12a4339a00b432d30b260111249115e346252330
Philipp <philipp.thiel@hispeed.ch>
2021-12-02 15:19:50 +0100

    Day 2

--- Day-02/src/main.java
+++ Day-02/src/main.java
@@ -4,9 +4,30 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws FileNotFoundException {
-        //Scanner sc = new Scanner(new File("main.in"));
-        Scanner sc = new Scanner(new File("test.in"));
+        Scanner sc = new Scanner(new File("main.in"));
+        //Scanner sc = new Scanner(new File("test.in"));
+        int hor = 0;
+        int vert = 0;
+        int aim = 0;
+        while (sc.hasNext()) {
+            String dir = sc.next();
+            int X = sc.nextInt();
 
-
+            switch (dir) {
+                case "forward":
+                    hor += X;
+                    vert += aim*X;
+                    break;
+                case "down":
+                    aim += X;
+                    break;
+                case "up":
+                    aim -= X;
+                    break;
+                default:
+                    System.out.println("wth");
+            }
+        }
+        System.out.println("res: "+hor*vert);
     }
 }
